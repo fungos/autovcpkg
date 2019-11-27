12f9d042aa976b2d8e2fd2d2530854a08ea154fc
Danny Angelo Carminati Grein <fungos@gmail.com>
2019-11-26 19:52:22 -0600

    Restructure examples to cover a wider range of possibilities
    Minor fixes on windows after introduction of vcpkg-rs

--- examples/curl_vcpkg/src/lib.rs
+++ examples/curl_vcpkg/src/lib.rs
@@ -5,28 +5,14 @@ mod ffi {
     use std::os::raw::c_char;
 
     extern "C" {
-        pub fn zlibVersion() -> *const c_char;
         pub fn curl_version() -> *const c_char;
     }
 }
 
-fn zlib_version() {
-    unsafe {
-        let ptr: *const c_char = ffi::zlibVersion();
-        let c_str = CStr::from_ptr(ptr);
-        println!("zlib version: {}", c_str.to_string_lossy().to_string());
-    }
-}
-
-fn curl_version() {
+pub fn version() {
     unsafe {
         let ptr: *const c_char = ffi::curl_version();
         let c_str = CStr::from_ptr(ptr);
         println!("curl version: {}", c_str.to_string_lossy().to_string());
     }
-}
-
-fn main() {
-    zlib_version();
-    curl_version();
 }
